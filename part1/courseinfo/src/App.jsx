e8433c65dd4ddb1a21083667ea8c1a66d5941ebe
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-10-25 15:17:11 +0200

    ex1.2_part1_finished

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,6 +7,16 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const Part = (props) => {
+  return (
+      <div>
+        <p>
+            {props.part} {props.exercise}
+        </p>
+      </div>
+    )
+  }
+
   const Header = (props) => {
   return (
       <h1>{props.course}</h1>
@@ -15,17 +25,11 @@ const App = () => {
 
   const Content = (props) => {
   return (
-      <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
-      </>
+      <div>
+        <Part part={props.part1} exercise={props.exercises1} />
+        <Part part={props.part2} exercise={props.exercises2} />
+        <Part part={props.part3} exercise={props.exercises3} />
+      </div>
     )
   }
 

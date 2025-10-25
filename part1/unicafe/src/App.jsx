1b42dfcf2aea56c0de5665bcd01cf9d42ea80d3a
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-10-26 00:24:23 +0200

    ex1.10_part1_finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 
 const DisplayTitle = ({title}) => <h1>{title}</h1>
-const DisplayValue = ({text, value}) => <p>{text} {value}</p>
+
+
+
+const StatisticLine = (props) => <p>{props.text} {props.value}</p>
 
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
@@ -9,12 +12,15 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
-const Statistics = ({good,neutral,bad}) => {
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+
   const total = good + neutral + bad
 
-  const Average = ({good, neutral, bad, total}) => (good * 1 + bad * (-1)) / total
-  const Positive = ({good, neutral, bad, total}) => good * 100 / total
-
+  const averageValue = (good * 1 + bad * (-1)) / total
+  const positivePercentage = good * 100 / total
 
   if (total === 0) {
     return <p>No feedback given</p>
@@ -22,13 +28,14 @@ const Statistics = ({good,neutral,bad}) => {
 
   return (
     <>
-      <DisplayTitle title="statistics"/>
-      <DisplayValue text="good" value={good} />
-      <DisplayValue text="neutral" value={neutral} />
-      <DisplayValue text="bad" value={bad} />
-      <DisplayValue text="all" value={good + neutral + bad} />
-      <DisplayValue text="average" value={Average({good,neutral,bad, total})} />
-      <DisplayValue text="positive" value={Positive({good,neutral,bad, total}) + "%"} />
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={averageValue} />
+
+      <StatisticLine text="positive" value={positivePercentage + "%"} />
     </>
   )
 }
@@ -38,13 +45,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
   return (
     <div>
       <DisplayTitle title="give feedback"/>
       <Button onClick={() => setGood(good+1)} text="good"/>
       <Button onClick={() => setNeutral(neutral+1)} text="neutral"/>
       <Button onClick={() => setBad(bad+1)} text="bad"/>
+      <DisplayTitle title="statistics"/>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )

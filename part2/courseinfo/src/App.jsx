c0338b5560a2e07e5d92915f15a6acc0b7f5e6ba
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-10-26 16:40:54 +0100

    ex2.5_part2_finished

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 const Curriculum = ({courses}) => {
   return (
     <>
@@ -5,32 +7,6 @@ const Curriculum = ({courses}) => {
     </>
   )
 }
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = ({ name }) => <h1>{name}</h1>
-
-const Content = ({ parts }) => {
-  const total = parts.reduce((accumulator, parts) => {
-    return accumulator + parts.exercises;
-  }, 0);
-
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-      <p><b>total of {total} exercises</b></p>
-    </>
-  )
-}
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const App = () => {
   const courses = [

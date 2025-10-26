b24354e57fbc5052e4151ff10d6fdb1e53ef9573
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-10-26 20:55:32 +0100

    ex2.8_part2_finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 
 const Persons = ({persons}) => persons.map(person => <Person key={person.name} person={person} />)
 
-const Person = ({person}) => <p>{person.name}</p>
+const Person = ({person}) => <p>{person.name} {person.number}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleInputChange = (event) => {
-    console.log(event.target.value)
+  const handleNameInputChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handlePhoneNumberInputChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const onClick = (event) => {
@@ -21,8 +25,11 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     }
+    else if (persons.some(number => number.number === newNumber)) {
+      alert(`${newNumber} is already added to phonebook`)
+    }
     else {
-      const newPersons = persons.concat({name: newName})
+      const newPersons = persons.concat({name: newName, number: newNumber})
       setPersons(newPersons)
     }
   }
@@ -32,16 +39,12 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input onChange={handleInputChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={onClick}>add</button>
-        </div>
+        <div>name: <input onChange={handleNameInputChange} /></div>
+        <div>number: <input onChange={handlePhoneNumberInputChange} /></div>
+        <div><button type="submit" onClick={onClick}>add</button></div>
       </form>
       <h2>Numbers</h2>
       <Persons persons={persons} />
-    <div>debug: {newName}</div>
     </div>
   )
 }

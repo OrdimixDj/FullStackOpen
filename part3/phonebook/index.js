5b0e5d07eb02e0a29e7ddd84c2eac158fdd58fdd
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-10-29 02:03:09 +0100

    ex3.3_part3_finished

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,17 +36,18 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebook)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => person.id === id)
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {

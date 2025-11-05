90cfe924da641564b285abda1ce6fb498978c52a
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-11-05 22:15:04 +0100

    ex4.13_part4_finished

--- part4/bloglist/tests/blog_api_test.test.js
+++ part4/bloglist/tests/blog_api_test.test.js
@@ -13,98 +13,129 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are correctly returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+  test('blogs are correctly returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blogs has an id an not an _id', async () => {
+    const response = await api.get('/api/blogs')
+    const firstBlog = response.body[0]
+    expect(firstBlog.id).toBeDefined()
+  })
 })
 
-test('blogs has an id an not an _id', async () => {
-  const response = await api.get('/api/blogs')
-  const firstBlog = response.body[0]
-  expect(firstBlog.id).toBeDefined()
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Title test',
+      author: 'Author test',
+      url: 'Url test',
+      likes: 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain(
+      'Title test'
+    )
+
+    const authors = blogsAtEnd.map(b => b.author)
+    expect(authors).toContain(
+      'Author test'
+    )
+
+    const urls = blogsAtEnd.map(b => b.url)
+    expect(urls).toContain(
+      'Url test'
+    )
+
+    const totLikes = blogsAtEnd.map(b => b.likes)
+    expect(totLikes).toContain(0)
+  })
+
+  test('works if likes is missing, with default value 0', async () => {
+    const newBlog = {
+      title: 'Title test',
+      author: 'Author test',
+      url: 'Url test'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const likes = blogsAtEnd.map(b => b.likes)
+    expect(likes).toContain(0) // 0 is supposed to be associated to that blog because the other blogs have 7 & 5 likes
+  })
+
+  test('fails with status code 400 if title or url missing', async () => {
+    const blogWithoutTitle = {
+      author: 'Author test',
+      url: 'Url test',
+      likes: 0
+    }
+
+    const blogWithoutUrl = {
+      title: 'Title test',
+      author: 'Author test',
+      likes: 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutTitle)
+      .expect(400)
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutUrl)
+      .expect(400)
+  })
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Title test',
-    author: 'Author test',
-    url: 'Url test',
-    likes: 0
-  }
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if it is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const blogsAtEnd = await helper.blogsInDb()
 
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain(
-    'Title test'
-  )
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
 
-  const authors = blogsAtEnd.map(b => b.author)
-  expect(authors).toContain(
-    'Author test'
-  )
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).not.toContain(blogToDelete.title)
 
-  const urls = blogsAtEnd.map(b => b.url)
-  expect(urls).toContain(
-    'Url test'
-  )
+    const authors = blogsAtEnd.map(b => b.author)
+    expect(authors).not.toContain(blogToDelete.author)
 
-  const totLikes = blogsAtEnd.map(b => b.likes)
-  expect(totLikes).toContain(0)
-})
+    const urls = blogsAtEnd.map(b => b.url)
+    expect(urls).not.toContain(blogToDelete.url)
 
-test('a blog without any like is added with 0 like', async () => {
-  const newBlog = {
-    title: 'Title test',
-    author: 'Author test',
-    url: 'Url test'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  const likes = blogsAtEnd.map(b => b.likes)
-  expect(likes).toContain(0) // 0 is supposed to be associated to that blog because the other blogs have 7 & 5 likes
-})
-
-test('a blog without title or url or both returns error 400', async () => {
-  const blogWithoutTitle = {
-    author: 'Author test',
-    url: 'Url test',
-    likes: 0
-  }
-
-  const blogWithoutUrl = {
-    title: 'Title test',
-    author: 'Author test',
-    likes: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutTitle)
-    .expect(400)
-
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutUrl)
-    .expect(400)
+    const likes = blogsAtEnd.map(b => b.likes)
+    expect(likes).not.toContain(blogToDelete.likes)
+  })
 })
 
 afterAll(async () => {

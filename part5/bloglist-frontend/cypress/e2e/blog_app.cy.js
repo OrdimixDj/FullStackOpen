1d1e2bd360418b78fbd81417bc6756d4d4d5ed71
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-11-08 19:42:01 +0100

    ex5.18_part5_finished

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,32 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {username: "test_user", name: "TEST", password: "testpassword"}
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('test_user')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+
+      cy.get('html').should('not.contain', 'Log in to application')
+      cy.contains('TEST logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('salainen')
+      cy.contains('login').click()
+
+      cy.contains('Log in to application')
+      cy.contains('wrong username or password')
+    })
   })
 })

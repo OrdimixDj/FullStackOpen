4ea617f6f8fb3cd7982c19a4ee3fb853a18544b1
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-11-08 19:24:26 +0100

    ex5.17_part5_finished

--- part5/bloglist-frontend/.eslintrc.cjs
+++ part5/bloglist-frontend/.eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
     browser: true,
     es2020: true,
     "jest/globals": true,
-    "jest": true
+    "jest": true,
+    "cypress/globals": true
   },
   extends: [
     'eslint:recommended',
@@ -15,7 +16,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'jest'],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     "indent": [
         "error",

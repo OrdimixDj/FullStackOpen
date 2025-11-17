e95c10bffc1c9ce7bdb3b1c7336acb8030b22c2c
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-11-18 00:30:37 +0100

    ex8.2_part8_finished

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -93,19 +93,25 @@ let books = [
   },
 ];
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }`;
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 

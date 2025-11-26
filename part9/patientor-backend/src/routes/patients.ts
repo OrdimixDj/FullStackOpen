a3c4304862bfb8b456c3dadc697f7bb4b2ec4765
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-11-26 22:55:11 +0100

    ex9.12_part9_finished

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -2,11 +2,30 @@ import express from 'express';
 import { Response } from 'express';
 import patientsService from '../services/patientsService';
 import { Patient } from "../types";
+import { toNewPatientEntry } from '../utils';
+
 
 const router = express.Router();
+
+router.post('/', (req, res) => {
+  try {
+    const newPatientEntry = toNewPatientEntry(req.body);
+
+    const addedEntry = patientsService.addPatient(newPatientEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+});
+
 
 router.get('/', (_req, res: Response<Patient[]>) => {
   res.send(patientsService.getNonSensitiveEntries());
 });
+
 
 export default router;
--- part9/patientor-backend/src/services/patientsService.ts
+++ part9/patientor-backend/src/services/patientsService.ts
@@ -1,22 +1,36 @@
+import { v1 as uuid } from "uuid";
 import patientsData from '../../data/patients';
-import { Patient, NonSensitivePatientEntry } from "../types";
+import { Patient, NonSensitivePatientEntry, NewPatientEntry } from "../types";
 
 const getEntries = (): Patient[] => {
   return patientsData;
 };
 
 const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
-  return patientsData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+  return patientsData.map(({ id, name, dateOfBirth, ssn, gender, occupation }) => ({
   id,
   name,
   dateOfBirth,
+  ssn,
   gender,
   occupation
   }));
 };
 
+const addPatient = ( entry: NewPatientEntry ): Patient => {
+  const newPatientEntry = {
+    id: uuid(),
+    ...entry
+  };
+
+  patientsData.push(newPatientEntry);
+  return newPatientEntry;
+};
+
+
 
 export default {
   getEntries,
-  getNonSensitiveEntries
+  getNonSensitiveEntries,
+  addPatient
 };

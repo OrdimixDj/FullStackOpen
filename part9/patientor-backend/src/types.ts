698236fd2734bcb7fef3b8a003548d596acecc16
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-11-28 23:17:31 +0100

    ex9.23_part9_finished

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,20 +1,62 @@
 import { z } from 'zod';
 import { NewEntrySchema } from './utils';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+export interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export enum Gender {
   Male = 'male',
   Female = 'female',
   Other = 'other',
-}
-
-export interface Diagnose {
-  code: string;
-  name: string;
-  latin?: string;
 }
 
 export interface Patient {
